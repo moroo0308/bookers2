6d2231d4cae93171d2350a100fd0610ef4decfad
USER <user@Tatsuki-MacBook-Air.local>
2018-10-25 12:27:07 +0900

    [Finish]5-9

--- bookers3/app/controllers/books_controller.rb
+++ bookers3/app/controllers/books_controller.rb
@@ -1,17 +1,12 @@
 class BooksController < ApplicationController
+	before_action :correct_user, only: [:edit, :update]
 	def index
 		@book = Book.new
 		@books = Book.all
-		
-	end
-	def new
-		@book = Book.new
-		
 	end
 	def show
 		@book = Book.find(params[:id])
-		
-		
+		@book1 = Book.new
 	end
 	def edit
 		@book = Book.find(params[:id])
@@ -36,5 +31,11 @@ class BooksController < ApplicationController
 	def book_params
 		params.require(:book).permit(:title, :opinion, :image)
 	end
+	def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path
+    end
+  	end
 
 end
--- bookers3/app/controllers/users_controller.rb
+++ bookers3/app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
+	before_action :correct_user, only: [:edit, :update]
 	def index
 		@users = User.all
 		@user = User.new
 		@book = Book.new
-		
 	end
-	
 	def edit
 		@user = User.find(params[:id])
 	end
 	def show
 		@user = User.find(params[:id])
 		@book = Book.new
+		@user1 = User.find(params[:id])
 	end
 	def update
 		user = User.find(params[:id])
@@ -23,4 +23,10 @@ class UsersController < ApplicationController
 		params.require(:user).permit(:name, :image, :introduction)	 
 		                           # :introduction追加
 	end
+	def correct_user
+    user = User.find(params[:id])
+    if current_user != user
+      redirect_to user_path
+    end
+  end
 end
