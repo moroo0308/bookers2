e532db09930a02c9114b2a9855c3f8aab84550f2
USER <user@Tatsuki-MacBook-Air.local>
2018-10-25 12:30:56 +0900

    [Finish]5-9

--- Bookers/app/controllers/top_controller.rb
+++
@@ -1,9 +0,0 @@
-class TopController < ApplicationController
-	
-	def index
-		
-	end
-	def new
-		
-	end
-end
--- Bookers/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  
-  devise_for :users
-  root 'top#index'
-  resources :books, only: [:new, :create, :index, :show]
-  resources :users, only: [:new, :create, :index, :show]
-  resources :top, only: [:new, :create, :index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
